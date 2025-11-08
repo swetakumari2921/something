1cb55939e5a336e8b894efb925b2c325cea9271a
swetakumari2921 <swetaamouna@gmail.com>
2025-11-08 08:05:28 +0530

    second commit

--- src/com/github/session/Demo.java
+++ src/com/github/session/Demo.java
@@ -2,6 +2,6 @@ package com.github.session;
 
 public class Demo {
 	public static void main(String[] args) {
-		System.out.println("Hello Java Good");
+		System.out.println("Hello Java Good Morning");
 	}
 }
